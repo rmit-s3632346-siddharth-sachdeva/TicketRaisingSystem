137d222665d532cb148aafa59c3461aa3ccfedb7
rmit-s3632346-siddharth-sachdeva <s3632346@student.rmit.edu.au>
2017-08-09 21:16:57 +1000

    Changes in master blade file.

---
+++ database/migrations/app_migrations/2017_08_09_102001_create_tickets_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTicketsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->increments('ticket_id');
+            $table->string('user_id');
+            $table->string('subject');
+            $table->string('priority');
+            $table->string('department');
+            $table->string('preferred_contact');
+            $table->string('operating_system');
+            $table->string('issue_description');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tickets');
+    }
+}
--- database/migrations/app_migrations/2017_08_09_102715_create_comments_table.php
+++ database/migrations/app_migrations/2017_08_09_102715_create_comments_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTicketsTable extends Migration
+class CreateCommentsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,11 @@ class CreateTicketsTable extends Migration
      */
     public function up()
     {
-        Schema::create('tickets', function (Blueprint $table) {
-            $table->increments('id');
+        Schema::create('comments', function (Blueprint $table) {
+            $table->increments('comment_id');
+            $table->string('ticket_id');
+            $table->string('user_id');
+            $table->string('description');
             $table->timestamps();
         });
     }
@@ -26,6 +29,6 @@ class CreateTicketsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('tickets');
+        Schema::dropIfExists('comments');
     }
 }
