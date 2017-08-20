e1a1783aed5a7dcec426a501afbe769795f9f955
rmit-s3632346-siddharth-sachdeva <s3632346@student.rmit.edu.au>
2017-08-20 15:21:56 +1000

    Table view changes

--- resources/views/shared/ticketList.blade.php
+++ resources/views/shared/ticketList.blade.php
@@ -3,8 +3,11 @@
         <div class="listtable">
             <div id="tableTicketsList_filter" class="dataTables_filter">
                 <label>
-                    <input type="search" class="form-control input-sm" placeholder="Enter search term..." aria-controls="tableTicketsList">
-                </label></div><div class="dataTables_info" id="tableTicketsList_info" role="status" aria-live="polite">Showing 1 to 10 of 28 entries
+                    <input type="search" class="form-control input-sm" placeholder="Enter search term..."
+                           aria-controls="tableTicketsList">
+                </label></div>
+            <div class="dataTables_info" id="tableTicketsList_info" role="status" aria-live="polite">Showing 1 to 7
+                of {{$ticketList->total()}} entries
             </div>
             <table class="table table-striped table-hover table-list dataTable no-footer dtr-inline">
                 <thead>
@@ -18,45 +21,46 @@
                 </thead>
                 <tbody>
                 @foreach($ticketList as $ticket)
-                <tr class="active">
-                    <td>{{ $ticket->ticketId}}</td>
-                    <td>{{ $ticket->subject}}</td>
-                    <td>{{ $ticket->serviceArea}}</td>
-                    <td>
-                        @if($ticket->priority == 'H')
-                            High
-                            @elseif($ticket->priority == 'M')
-                            Medium
-                            @else
-                            Low
-                        @endif
-                    </td>
-                    <td>{{ $ticket->status}}</td>
-                    <td><a class="btn btn-info" href="{{ route('viewTickets.show',$ticket->ticketId) }}">Show</a></td>
-                    {{--<td><a class="btn btn-primary" href="--}}{{--{{ route('productCRUD.edit',$product->id) }}--}}{{--">Edit</a></td>--}}
-
-
-                    @if($ticket->status == 'Closed')
-                        <td>  {!! Form::open(['method' => 'PATCH','route' => ['viewTickets.update', $ticket->ticketId ],'style'=>'display:inline']) !!}
-                        {!! Form::submit('Re-open Ticket', ['class' => 'btn btn-danger']) !!}</td>
-                        {!! Form::close() !!}
-                        {{--<td><a class="btn btn-danger" disabled --}}{{--href="{{ route('viewTickets.update', $ticket) }}"--}}{{-->Ticket Closed</a></td>--}}
-                    @else
-
+                    <tr class="active">
+                        <td>{{ $ticket->ticketId}}</td>
+                        <td>{{ $ticket->subject}}</td>
+                        <td>{{ $ticket->serviceArea}}</td>
                         <td>
-                            {!! Form::open(['method' => 'PATCH','route' => ['viewTickets.update', $ticket->ticketId ],'style'=>'display:inline']) !!}
+                            @if($ticket->priority == 'H')
+                                High
+                            @elseif($ticket->priority == 'M')
+                                Medium
+                            @else
+                                Low
+                            @endif
+                        </td>
+                        <td>{{ $ticket->status}}</td>
+                        <td><a class="btn btn-info" href="{{ route('viewTickets.show',$ticket->ticketId) }}">Show</a>
+                        </td>
+                        {{--<td><a class="btn btn-primary" href="--}}{{--{{ route('productCRUD.edit',$product->id) }}--}}{{--">Edit</a></td>--}}
 
-                            {!! Form::submit('Close Ticket', ['class' => 'btn btn-success']) !!}</td>
+
+                        @if($ticket->status == 'Closed')
+                            <td>  {!! Form::open(['method' => 'PATCH','route' => ['viewTickets.update', $ticket->ticketId ],'style'=>'display:inline']) !!}
+                                {!! Form::submit('Re-open Ticket', ['class' => 'btn btn-danger']) !!}</td>
+                            {!! Form::close() !!}
+                            {{--<td><a class="btn btn-danger" disabled --}}{{--href="{{ route('viewTickets.update', $ticket) }}"--}}{{-->Ticket Closed</a></td>--}}
+                        @else
+
+                            <td>
+                                {!! Form::open(['method' => 'PATCH','route' => ['viewTickets.update', $ticket->ticketId ],'style'=>'display:inline']) !!}
+
+                                {!! Form::submit('Close Ticket', ['class' => 'btn btn-success']) !!}</td>
 
                             {!! Form::close() !!}
 
-{{--                        {!! Form::model($ticket, ['method' => 'PATCH','route' => ['viewTickets.update', $ticket->ticketId]]) !!}--}}
+                            {{--                        {!! Form::model($ticket, ['method' => 'PATCH','route' => ['viewTickets.update', $ticket->ticketId]]) !!}--}}
 
 
-                    @endif
-                </tr>
+                        @endif
+                    </tr>
                 @endforeach
-                </tbody >
+                </tbody>
             </table>
             {!! $ticketList->render() !!}
         </div>
--- resources/views/shared/ticketList_admin.blade.php
+++ resources/views/shared/ticketList_admin.blade.php
@@ -3,8 +3,11 @@
         <div class="listtable">
             <div id="tableTicketsList_filter" class="dataTables_filter">
                 <label>
-                    <input type="search" class="form-control input-sm" placeholder="Enter search term..." aria-controls="tableTicketsList">
-                </label></div><div class="dataTables_info" id="tableTicketsList_info" role="status" aria-live="polite">Showing 1 to 10 of 28 entries
+                    <input type="search" class="form-control input-sm" placeholder="Enter search term..."
+                           aria-controls="tableTicketsList">
+                </label></div>
+            <div class="dataTables_info" id="tableTicketsList_info" role="status" aria-live="polite">Showing 1 to 7
+                of {{$ticketList->total()}} entries
             </div>
             <table class="table table-striped table-hover table-list dataTable no-footer dtr-inline">
                 <thead>
@@ -18,50 +21,51 @@
                 </thead>
                 <tbody>
                 @foreach($ticketList as $ticket)
-                <tr class="active">
-                    <td>{{ $ticket->ticketId}}</td>
-                    <td>{{ $ticket->subject}}</td>
-                    <td>{{ $ticket->serviceArea}}</td>
-                    <td>
-                        @if($ticket->priority == 'H')
-                            High
+                    <tr class="active">
+                        <td>{{ $ticket->ticketId}}</td>
+                        <td>{{ $ticket->subject}}</td>
+                        <td>{{ $ticket->serviceArea}}</td>
+                        <td>
+                            @if($ticket->priority == 'H')
+                                High
                             @elseif($ticket->priority == 'M')
-                            Medium
+                                Medium
                             @else
-                            Low
-                        @endif
-                    </td>
-                    <td>{{ $ticket->status}}</td>
-                    <td><a class="btn btn-info" href="{{ route('viewTickets.show',$ticket->ticketId) }}">Show</a></td>
-
-{{--
-                    @if($isEditable != true)
-
-                        <td>
-                            {!! Form::open(array('route' => 'setEditable','style'=>'display:inline')) !!}
-                            {{ Form::hidden('ticketObject', $ticket) }}
-                            {!! Form::submit('Edit', ['class' => 'btn btn-danger']) !!}
-                        {!! Form::close() !!}
-
-
+                                Low
+                            @endif
+                        </td>
+                        <td>{{ $ticket->status}}</td>
+                        <td><a class="btn btn-info" href="{{ route('viewTickets.show',$ticket->ticketId) }}">Show</a>
                         </td>
 
-                    @else
+                        {{--
+                                            @if($isEditable != true)
 
-                        <td>
-
-                            {!! Form::open(array('route' => 'performUpdate','style'=>'display:inline')) !!}
-                            {!! Form::submit('Done', ['class' => 'btn btn-danger']) !!}</td>
-                        {!! Form::close() !!}
-
-                        </td>
-
+                                                <td>
+                                                    {!! Form::open(array('route' => 'setEditable','style'=>'display:inline')) !!}
+                                                    {{ Form::hidden('ticketObject', $ticket) }}
+                                                    {!! Form::submit('Edit', ['class' => 'btn btn-danger']) !!}
+                                                {!! Form::close() !!}
 
 
-                    @endif--}}
-                </tr>
+                                                </td>
+
+                                            @else
+
+                                                <td>
+
+                                                    {!! Form::open(array('route' => 'performUpdate','style'=>'display:inline')) !!}
+                                                    {!! Form::submit('Done', ['class' => 'btn btn-danger']) !!}</td>
+                                                {!! Form::close() !!}
+
+                                                </td>
+
+
+
+                                            @endif--}}
+                    </tr>
                 @endforeach
-                </tbody >
+                </tbody>
             </table>
             {!! $ticketList->render() !!}
         </div>
