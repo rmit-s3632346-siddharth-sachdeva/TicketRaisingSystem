88d61858e9a30b03a679cafee2913d6848c017f5
rmit-s3632346-siddharth-sachdeva <s3632346@student.rmit.edu.au>
2017-08-20 20:01:16 +1000

    Formatting changes.

--- resources/views/email/TicketRaised.blade.php
+++ resources/views/email/TicketRaised.blade.php
@@ -1,3 +1,6 @@
+<html>
+
+<body>
 <h1>Ticket Raised</h1>
 <h3>{{Session::get('success')}}</h3>
 </br>
@@ -7,3 +10,6 @@ Administrator | RMIT Support & Service</br>
 RMIT University - Success begins here</br>
 M: +61 411 333 222</br>
 E: admin@rmit.edu.au</p>
+
+</body>
+</html>
